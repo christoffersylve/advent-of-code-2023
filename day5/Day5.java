9e38c2738fd5308b91a142f1fc356f7f3f134d7e
Christoffer Sylve <christoffer.sylve@outlook.com>
2023-12-05 19:43:58 +0100

    day5, problem 2 solved

--- day5/Day5.java
+++ day5/Day5.java
@@ -8,10 +8,20 @@ public class Day5 {
 
     static ArrayList<Long> seeds = new ArrayList<>();
     static ArrayList<ArrayList<Long[]>> mappings = new ArrayList<>();
+    static ArrayList<Long> start = new ArrayList<>(); 
+    static ArrayList<Long> end = new ArrayList<>();
 
     public static void main(String[] args) throws FileNotFoundException {
-        readFromFile("day5/input.txt");
+        long start = System.currentTimeMillis();
+        readFromFile("input.txt");
+        System.out.println("readFromFile(): " + (System.currentTimeMillis()-start) + " ms");
+        start = System.currentTimeMillis();
         problem1();
+        System.out.println("Problem 2: " + 26714516);
+        System.out.println("problem1(): " + (System.currentTimeMillis()-start) + " ms");
+        start = System.currentTimeMillis();
+        problem2();
+        System.out.println("problem2(): " + (System.currentTimeMillis()-start) + " ms");
     }
 
     private static void readFromFile(String filePath) throws FileNotFoundException {
@@ -58,7 +68,33 @@ public class Day5 {
         System.out.println("Problem 1: " + minLocation);
     }
 
+    private static void problem2() {
+        long minLocation = (long) Long.MAX_VALUE;
+        long newMapping= (long) 0;
+        long to = 0;
+        long from = 0;
+        long range = 0;
 
+        for(int r=0; r<seeds.size(); r+=2) {
+            //System.out.println("Progress: " + (r*10) + "%");
+            long start = seeds.get(r);
+            long end = seeds.get(r+1);
+            for(long s = start; s<start+end; s++) { 
+                newMapping = s;
+                for(ArrayList<Long[]> mapping : mappings) {
+                    for(int i=0;i<mapping.size();i++) {
+                        to = mapping.get(i)[0];
+                        from = mapping.get(i)[1];
+                        range = mapping.get(i)[2];
+                        if(newMapping >= from && newMapping <= (range+from)) {
+                            newMapping = newMapping - from + to;
+                            break;
+                        } 
+                    }
+                }
+                minLocation = (newMapping < minLocation) ? newMapping : minLocation;
+            }
+        }
+        System.out.println("Problem 2: " + minLocation);
+    }
 }
-
-    
